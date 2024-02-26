9e47bf53a32b414a210ff05392caf09548218050
YimingHao0730 <97846459+YimingHao0730@users.noreply.github.com>
2024-02-25 21:13:26 -0800

    Update getorf.py

--- scripts/getorf.py
+++ scripts/getorf.py
@@ -1,29 +1,33 @@
 import os
 import subprocess
 
-# Input file containing the names of the FASTA files
+# Input file containing the names of the FASTQ files
 in_file = "fasta_sequence_names.txt"
 
 # Open the input file and read each line
 with open(in_file, "r") as file:
     for line in file:
         # Clean up the line to remove newline characters
-        fasta_name = line.strip()
+        fastq_name = line.strip()
 
         # Construct the output file name
-        out_name = f"{fasta_name}.fa"
+        fasta_name = f"{fastq_name.split('.')[0]}.fasta"
+        
+        decompressed_file = fastq_name.replace('.gz', '')
 
-        # Decompress the FASTA file
-        decompressed_file = fasta_name.replace('.gz', '')
-
-        decompression_cmd = f"gzip -d fasta_sequence_path/{fasta_name}"
+        # Decompress the FASTQ file
+        decompression_cmd = f"gzip -d fasta_sequence_path/{fastq_name}"
         subprocess.run(decompression_cmd, shell=True)
+
+        # Convert FASTQ to FASTA using EMBOSS
+        conversion_cmd = f"seqret -sequence fasta_sequence_path/{decompressed_file} -outseq fasta_sequence_path/{fasta_name}"
+        subprocess.run(conversion_cmd, shell=True)
 
         # Construct the command to run getorf
         cmd = (
-            f"getorf -sequence fasta_sequence_path/{decompressed_file} "
+            f"getorf -sequence ../fasta_sequence_path/{fasta_name} "
             f"-find 0 -table 11 -minsize 15 -maxsize 150 "
-            f"-outseq ../Data/{out_name}"
+            f"-outseq ../Data/{fasta_name}"
         )
 
         # Execute the command
@@ -31,9 +35,10 @@ with open(in_file, "r") as file:
         # Uncomment the line below to print the command instead of executing
         # print(cmd)
 
-# Concatenate all the .fa files into one file called input.fa
+# Concatenate all the .fasta files into one file called input.fa
 output_dir = "../Data"
 input_fa_path = os.path.join(output_dir, "input.fa")
 
-cat_cmd = f"cat {output_dir}/*.fa > {input_fa_path}"
+cat_cmd = f"cat {output_dir}/*.fasta > {input_fa_path}"
 subprocess.run(cat_cmd, shell=True)
+
