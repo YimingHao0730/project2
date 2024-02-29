aeb907f4f41d044656615daa8d7a649477efcefe
YimingHao0730 <97846459+YimingHao0730@users.noreply.github.com>
2024-02-29 10:38:45 -0800

    Update trim.py

--- scripts/trim.py
+++ scripts/trim.py
@@ -1,7 +1,7 @@
 import argparse
 
 # Set up argument parsing
-parser = argparse.ArgumentParser(description='Keep the first 10,000 sequences of a FASTA file.')
+parser = argparse.ArgumentParser(description='Keep the first 10,000,000 sequences of a FASTA file.')
 parser.add_argument('input_file', type=str, help='Input FASTA file path')
 parser.add_argument('output_file', type=str, help='Output FASTA file path')
 
@@ -14,7 +14,7 @@ with open(args.input_file, 'r') as infile, open(args.output_file, 'w') as outfil
     for line in infile:
         if line.startswith('>'):
             count += 1
-            if count > 1000000:
+            if count > 10000000:
                 break
         outfile.write(line)
 print('trim end')
