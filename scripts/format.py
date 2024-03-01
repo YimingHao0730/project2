391e0d5356e4f4fa190819ececca643a1e304ce6
YimingHao0730 <97846459+YimingHao0730@users.noreply.github.com>
2024-03-01 14:54:11 -0800

    updated format.py

--- scripts/format.py
+++ scripts/format.py
@@ -7,22 +7,23 @@ def translate_sequence(in_file, out_file):
         'F': "5", 'G': "6", 'H': "7", 'I': "8",
         'K': "9", 'L': "10", 'M': "11", 'N': "12",
         'P': "13", 'Q': "14", 'R': "15", 'S': "16",
-        'T': "17", 'V': "18", 'W': "19", 'Y': "20",
+        'T': "17", 'V': "18", 'W':"19", 'Y':"20" 
     }
 
     with open(in_file, 'r') as infile, open(out_file, 'w') as outfile:
         for line in infile:
             line = line.strip().upper()
             if line.startswith('>'):
-                b = "0"
+                continue  # Skip further processing for header lines
             else:
-                # Skip invalid amino acids
-                if any(x in line for x in ['B', 'J', 'O', 'U', 'X', 'Z']):
-                    continue
-                else:
-                    seq = list(line)
-                    b = "0," * (300 - len(seq) - 1) + ",".join([aacode[aa] for aa in seq if aa in aacode])
-                    outfile.write(b + "\n")
+                # Generate sequence encoding
+                encoded_seq = ",".join([aacode[aa] for aa in line if aa in aacode])
+                # Calculate how much padding is needed
+                padding_needed = 300 - len(encoded_seq.split(','))
+                # Prepend the padding
+                b = "0," * padding_needed + encoded_seq
+                # Write the padded sequence to the outfile
+                outfile.write(b + "\n")
 
 if __name__ == "__main__":
     start_time = time.time()  # Start time
@@ -34,4 +35,3 @@ if __name__ == "__main__":
 
     end_time = time.time()  # End time
     print(f"Done. Execution time: {end_time - start_time:.2f} seconds.")
-
